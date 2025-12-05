6aba54015277d04520aa4bdb94460bd0b99260ca
Ilyasix <hfvvevdhdj@gmail.com>
2025-12-05 11:12:13 +0200

    chore: updating eslint config to prevent errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,18 @@
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsparser from '@typescript-eslint/parser';
-import prettier from 'eslint-config-prettier';
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettier from "eslint-config-prettier";
 
 export default [
-  { ignores: ['**/dist/**', '**/node_modules/**'] },
   {
-    files: ['**/*.ts', '**/*.js'],
+    ignores: ["**/node_modules/**", "**/dist/**", "**/generated/**", "**/prisma/migrations/**"],
+  },
+  {
+    files: ["**/*.ts", "**/*.js"],
     languageOptions: {
       parser: tsparser,
-      parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+      parserOptions: { ecmaVersion: "latest", sourceType: "module" },
     },
-    plugins: { '@typescript-eslint': tseslint },
+    plugins: { "@typescript-eslint": tseslint },
     rules: { ...tseslint.configs.recommended.rules },
   },
   prettier,
